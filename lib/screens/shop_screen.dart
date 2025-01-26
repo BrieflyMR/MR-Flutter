958727b3da68a2afae3f350a83457895d0c2559b
BrieflyMR <brieflymr@gmail.com>
2025-01-27 00:35:09 +0300

    Shop Screen Güncellendi

--- lib/screens/shop_screen.dart
+++ lib/screens/shop_screen.dart
@@ -1,5 +1,6 @@
 import 'package:dotlottie_loader/dotlottie_loader.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:lottie/lottie.dart';
 
 import '../core/constants.dart';
@@ -9,27 +10,92 @@ class ShopScreen extends StatelessWidget {
   const ShopScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final surface = lightColors["surface"]!;
-    return Scaffold(
-            backgroundColor: surface,
-      body:Expanded(
-            child: Container(
-              padding: const EdgeInsets.all(16),
-              child: SizedBox(
-                width: double.infinity,
-                child: DotLottieLoader.fromAsset("assets/motions/shopping.lottie",
-                      frameBuilder: (BuildContext ctx, DotLottie? dotlottie) {
-                    if (dotlottie != null) {
-                      return Lottie.memory(dotlottie.animations.values.single);
-                    } else {
-                      return Container();
-                    }
-                  }),
+Widget build(BuildContext context) {
+  return Scaffold(
+    body: ListView(
+      children: [
+        // Basit bir metin
+        Text("Merhaba Arkadaşlar"),
+
+        // İlk Inkwell örneği
+        InkWell(
+          onTap: () {
+            // Tıklama kodu buraya gelecek
+          },
+          splashColor: Theme.of(context).colorScheme.primary,
+          child: Row(
+            children: [
+              Icon(Icons.person, size: 38, color: Theme.of(context).colorScheme.primary),
+              const SizedBox(width: 5.0),
+              Text(
+                "Profil",
+                style: Theme.of(context).textTheme.displaySmall!.copyWith(
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
               ),
-            ),
+            ],
           ),
-        bottomNavigationBar: BottomMenu(),
-    );
-  }
+        ),
+
+        // İkinci Inkwell örneği
+        InkWell(
+          onTap: () {
+            // Tıklama kodu buraya gelecek
+          },
+          splashColor: Theme.of(context).colorScheme.error,
+          child: Row(
+            children: [
+              Icon(Icons.delete, size: 38, color: Theme.of(context).colorScheme.error),
+              const SizedBox(width: 5.0),
+              Text(
+                "Hesabı Sil",
+                style: Theme.of(context).textTheme.displaySmall!.copyWith(
+                      color: Theme.of(context).colorScheme.error,
+                    ),
+              ),
+            ],
+          ),
+        ),
+
+        // Inkwell ile tıklama ve uyarı diyaloğu
+        InkWell(
+          onTap: () {
+            showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                title: const Text("Uyarı"),
+                content: const Text("Silmekten emin misiniz?"),
+                actions: [
+                  IconButton(
+                    onPressed: () {
+                      // Onaylandı
+                    },
+                    icon: const Icon(Icons.check_circle),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      // Profili aç
+                      context.push("/profile");
+                    },
+                    icon: const Icon(Icons.cancel),
+                  ),
+                ],
+              ),
+            );
+          },
+          splashColor: Theme.of(context).colorScheme.primary,
+          child: Row(
+            children: [
+              const SizedBox(width: 5.0),
+              Text(
+                "Silme Uyarısı",
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+            ],
+          ),
+        ),
+      ],
+    ),
+  );
+}
 }
