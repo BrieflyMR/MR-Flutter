7bdf9971634874fc0f9266d0bc74218735b642a9
BrieflyMR <brieflymr@gmail.com>
2025-01-27 17:09:50 +0300

    Notfication Screen Düzenlenmek Üzere Boşaltıldı

--- lib/screens/notifications_screen.dart
+++ lib/screens/notifications_screen.dart
@@ -1,7 +1,4 @@
-import 'package:dotlottie_loader/dotlottie_loader.dart';
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
-
 import '../core/constants.dart';
 import '../widgets/bottom_menu.dart';
 
@@ -13,22 +10,11 @@ class NotificationsScreen extends StatelessWidget {
     final surface = lightColors["surface"]!;
     return Scaffold(
             backgroundColor: surface,
-      body:Expanded(
-            child: Container(
-              padding: const EdgeInsets.all(16),
-              child: SizedBox(
-                width: double.infinity,
-                child: DotLottieLoader.fromAsset("assets/motions/notifications2.lottie",
-                      frameBuilder: (BuildContext ctx, DotLottie? dotlottie) {
-                    if (dotlottie != null) {
-                      return Lottie.memory(dotlottie.animations.values.single);
-                    } else {
-                      return Container();
-                    }
-                  }),
-              ),
-            ),
-          ),
+      body:ListView(
+		children:[
+          Text("Sayfalar Arasi Geçiş")
+    ]
+      ),
         bottomNavigationBar: BottomMenu(),
     );
   }
