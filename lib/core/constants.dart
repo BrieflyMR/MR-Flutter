c0f512bcb7133642b3264a6b10556522cb20310c
BrieflyMR <brieflymr@gmail.com>
2025-01-28 22:41:28 +0300

    Uygulamanın Renkleri Ayarlandı

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,25 +1,32 @@
 import 'package:flutter/widgets.dart';
 
 const lightColors = {
-  "primary": Color.fromARGB(255, 33, 150, 243),
+  "primary": Color.fromARGB(255, 186, 85, 211), // Canlı mor
   "onPrimary": Color.fromARGB(255, 255, 255, 255),
-  "secondary": Color.fromARGB(255, 3, 169, 244),
+  "secondary": Color.fromARGB(255, 165, 42, 42), // Şarap kırmızısı
   "onSecondary": Color.fromARGB(255, 255, 255, 255),
-  "surface": Color.fromARGB(255, 238, 238, 238),
-  "onSurface": Color.fromARGB(255, 33, 33, 33),
-  "success": Color.fromARGB(255, 76, 175, 80),
-  "error": Color.fromARGB(255, 244, 67, 54),
+  "surface": Color.fromARGB(255, 248, 243, 255), // Hafif mor beyaz
+  "onSurface": Color.fromARGB(255, 51, 51, 51),
+  "success": Color.fromARGB(255, 121, 134, 203), // Açık mor mavi
+  "error": Color.fromARGB(255, 239, 83, 80),
   "onError": Color.fromARGB(255, 255, 255, 255),
 };
 
 const darkColors = {
-  "primary": Color.fromARGB(255, 33, 33, 33),
+  "primary": Color.fromARGB(255, 123, 31, 162), // Derin mor
   "onPrimary": Color.fromARGB(255, 255, 255, 255),
-  "secondary": Color.fromARGB(255, 48, 63, 159),
+  "secondary": Color.fromARGB(255, 136, 14, 79), // Koyu şarap rengi
   "onSecondary": Color.fromARGB(255, 255, 255, 255),
-  "surface": Color.fromARGB(255, 66, 66, 66),
+  "surface": Color.fromARGB(255, 33, 33, 33),
   "onSurface": Color.fromARGB(255, 255, 255, 255),
-  "success": Color.fromARGB(255, 76, 175, 80),
-  "error": Color.fromARGB(255, 244, 67, 54),
+  "success": Color.fromARGB(255, 102, 187, 106),
+  "error": Color.fromARGB(255, 183, 28, 28),
   "onError": Color.fromARGB(255, 255, 255, 255),
 };
+
+
+
+
+
+
+
